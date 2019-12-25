1985746aae69534390d439d4f0d582dcb07cff25
liuxincheng <1633696870@qq.com>
2019-12-25 16:25:58 +0800

    Thread 多线程测试

--- src/main/Test/thread/Thread_Demo.java
+++ src/main/Test/thread/Thread_Demo.java
@@ -3,6 +3,8 @@ package thread;
 
 import org.junit.Test;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Thread_Demo {
 
     public static boolean isStop = false;
@@ -10,17 +12,18 @@ public class Thread_Demo {
     @Test
     public void testThread() throws InterruptedException {
 
+        AtomicInteger i= new AtomicInteger();
         Thread t1 = new Thread(()->{
-            int i = 0;
             System.out.println(Thread.currentThread().getName()+"is stated!");
 
-            while (i<10){
-                System.out.println(i++);
+            while (!isStop){
+                System.out.println(i.getAndIncrement());
             }
         });
 
+        i.set(2);
         t1.start();
         t1.sleep(1000);
-
+        isStop=true;
     }
 }
